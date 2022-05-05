cafbd935a6c987aee45215f992ee5ff6ad3e8a02
roi2fille <bakongodior@gmail.com>
2022-05-05 18:26:29 +0200

    Modification class importateur BAKONGO Dior

---
+++ Pratique/Pratique/Class_Importateur.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+         private string IdCl;
+
+    public string IdCl1
+    {
+        get { return IdCl; }
+        set { IdCl = value; }
+    }
+    private string NomCl;
+
+    public string NomCl1
+    {
+        get { return NomCl; }
+        set { NomCl = value; }
+    }
+    private string EmailCl;
+
+    public string EmailCl1
+    {
+        get { return EmailCl; }
+        set { EmailCl = value; }
+    }
+    private string PhoneCl;
+
+    public string PhoneCl1
+    {
+        get { return PhoneCl; }
+        set { PhoneCl = value; }
+    }
+    // initial
+    public importateur()
+    {
+
+    }
+ // modification
+    public importateur(string id, string nom, string email, string phone)
+    {
+        this.IdCl = id;
+        this.NomCl = nom;
+        this.EmailCl = email;
+        this.PhoneCl = phone;
+    }
+    // suppression
+    public importateur(string id)
+    {
+        this.IdCl = id;
+    }
+}
+	}
+}
