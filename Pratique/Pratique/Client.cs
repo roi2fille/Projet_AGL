b3552dd28622e2522f80cdbf0bf5b43f4d76b546
Henock <hlungelehenock@gmail.com>
2022-05-05 02:34:06 +0200

    ajout de la classe Client

---
+++ Pratique/Pratique/Client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratique
+{
+    class Client
+    {
+        private string IdCl;
+
+        public string IdCl1
+        {
+            get { return IdCl; }
+            set { IdCl = value; }
+        }
+        private string NomCl;
+
+        public string NomCl1
+        {
+            get { return NomCl; }
+            set { NomCl = value; }
+        }
+        private string EmailCl;
+
+        public string EmailCl1
+        {
+            get { return EmailCl; }
+            set { EmailCl = value; }
+        }
+        private string PhoneCl;
+
+        public string PhoneCl1
+        {
+            get { return PhoneCl; }
+            set { PhoneCl = value; }
+        }
+        //constructeur initial
+        public Client()
+        {
+
+        }
+        //constructeur de modification
+        public Client(string id, string nom, string email, string phone) 
+        {
+            this.IdCl = id;
+            this.NomCl = nom;
+            this.EmailCl = email;
+            this.PhoneCl = phone;
+        }
+        //constructeur de suppression
+        public Client(string id)
+        {
+            this.IdCl = id;
+        }
+    }
+}
