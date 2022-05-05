3b6f52541abb3512655e47dccf3a31feb412d1de
roi2fille <bakongodior@gmail.com>
2022-05-05 18:43:09 +0200

    Modification class Articlr BALUME BARHULULA Steven

---
+++ Pratique/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		 //declaration des attributs
+        private string IdArt;
+
+    public string IdArt1
+    {
+        get { return IdArt; }
+        set { IdArt = value; }
+    }
+    private string DesArt;
+
+    public string DesArt1
+    {
+        get { return DesArt; }
+        set { DesArt = value; }
+    }
+    private float PrixArt;
+
+    public float PrixArt1
+    {
+        get { return PrixArt; }
+        set { PrixArt = value; }
+    }
+    //constructeur
+
+    public Article()
+    {
+
+    }
+    //constructeur de modification et enregistrement
+    public Article(string id, string des, float p)
+    {
+        this.IdArt = id;
+        this.DesArt = des;
+        this.PrixArt = p;
+    }
+
+    //constructeur de destruction
+    public Article(string id)
+    {
+        this.IdArt = id;
+    }
+
+
+} //declaration des attributs
+private string IdArt;
+
+public string IdArt1
+{
+    get { return IdArt; }
+    set { IdArt = value; }
+}
+private string DesArt;
+
+public string DesArt1
+{
+    get { return DesArt; }
+    set { DesArt = value; }
+}
+private float PrixArt;
+
+public float PrixArt1
+{
+    get { return PrixArt; }
+    set { PrixArt = value; }
+}
+//constructeur
+
+public Article()
+{
+
+}
+//constructeur de modification et enregistrement
+public Article(string id, string des, float p)
+{
+    this.IdArt = id;
+    this.DesArt = des;
+    this.PrixArt = p;
+}
+
+//constructeur de destruction
+public Article(string id)
+{
+    this.IdArt = id;
+}
+
+
+    }
+	}
+}
